db6078fde4a15a1ee902dcef8ff69a415ab5502a
adnanali4131 <adnanali4131@gmail.com>
2024-01-31 13:55:43 +0500

    reuse code

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,6 @@
-const express = require('express');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-const router = new express.Router();
-require('dotenv').config();
+const router = require('./router');
 
 // User Registration
 router.post('/users', async (req, res) => {
